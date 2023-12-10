7611ac9e864f6f598b49a57e073389bea80147f8
Depriest43 <depriest43@gmail.com>
2023-12-10 01:27:10 -0800

    update2 Picker

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,36 +10,32 @@
         "Top Gun",
         "Mortal Kombat"
     };
+        private void OnButtonAddNameClicked(object sender, EventArgs e)
+        {
+            string newName = editorAddName.Text;
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                namesList.Add(newName);
 
-    public MainPage()
+                myPicker.ItemsSource = null;
+                myPicker.ItemsSource = namesList;
+            }
+
+            private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+            {
+                int selectedIndex = myPicker.SelectedIndex;
+                string chosenName = namesList[selectedIndex];
+                if (!string.IsNullOrWhiteSpace(chosenName))
+                {
+                    labelDisplayChosenNameHere.Text = "You have chosen the name: " + chosenName;
+                }
+            }
+
+                public MainPage()
     {
         InitializeComponent();
 
         myPicker.ItemsSource = namesList;
-            private void OnButtonAddNameClicked(object sender, EventArgs e)
-            {
-                string newName = editorAddName.Text;
-                if (!string.IsNullOrWhiteSpace(newName))
-                {
-                    namesList.Add(newName);
-
-                    myPicker.ItemsSource = null;
-                    myPicker.ItemsSource = namesList;
-                }
-                private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
-                {
-                    int selectedIndex = myPicker.SelectedIndex;
-                    string chosenName = namesList[selectedIndex];
-                    if (!string.IsNullOrWhiteSpace(chosenName))
-                    {
-                        labelDisplayChosenNameHere.Text = "You have chosen the name: " + chosenName;
-                    }
-
-                }
-            }
-
-
-
     }//main 
 
 }//class
